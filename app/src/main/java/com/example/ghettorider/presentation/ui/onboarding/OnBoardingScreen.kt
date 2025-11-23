ce3959b8bff7b9d24e00cfad3bdea12e36032cae
joaquincuenca <146710300+joaquincuenca@users.noreply.github.com>
2025-11-23 10:48:29 +0800

    💄 Fixed UI design

--- app/src/main/java/com/example/ghettorider/presentation/ui/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/example/ghettorider/presentation/ui/onboarding/OnBoardingScreen.kt
@@ -1,5 +1,6 @@
 package com.example.ghettorider.presentation.ui.onboarding
 
+import android.app.Activity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -13,85 +14,71 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.example.ghettorider.R
 
 @Composable
 fun OnboardingScreen1(onAdminClick: () -> Unit = {}) {
-    Scaffold(
-        topBar = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 16.dp)
-                    .padding(16.dp),
-                contentAlignment = Alignment.TopEnd
-            ) {
-                Text(
-                    text = "ADMIN",
-                    style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
-                    color = Color.White,
-                    modifier = Modifier
-                        .clickable { onAdminClick() }
-                        .padding(8.dp)
-                )
-            }
-        },
-        bottomBar = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 64.dp)
-                    .padding(horizontal = 16.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Column{
-                    Button(
-                        onClick = { /*TODO Implement Booking*/ },
-                        modifier = Modifier.fillMaxWidth(),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color(0xFFFFD700),
-                            contentColor = Color.Black
-                        ),
-                        shape = RoundedCornerShape(16.dp),
-                    ) {
-                        Text(
-                            text = "Book a Ride",
-                            modifier = Modifier.padding(vertical = 8.dp),
-                            style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
-                        )
-                    }
-                }
-            }
-        }
+    val view = LocalView.current
+    val window = (view.context as Activity).window
 
-    ) { innerPadding ->
+    SideEffect {
+        window.statusBarColor = Color.Black.toArgb()
+        WindowCompat.getInsetsController(window, view).apply {
+            isAppearanceLightStatusBars = false // White icons
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .statusBarsPadding()
+                .padding(16.dp)
+                .align(Alignment.TopEnd)
+        ) {
+            Text(
+                text = "ADMIN",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                color = Color.White,
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .clickable { onAdminClick() }
+                    .padding(8.dp)
+            )
+        }
 
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.onSurface)
-                .padding(innerPadding)
-                .padding(16.dp),
+                .padding(horizontal = 24.dp)
+                .padding(bottom = 140.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-
             Image(
                 painter = painterResource(id = R.drawable.main_picture),
                 contentDescription = "avatar1",
@@ -105,7 +92,7 @@ fun OnboardingScreen1(onAdminClick: () -> Unit = {}) {
                     )
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(24.dp))
 
             Text(
                 text = "Ride the Streets, Ghetto Style",
@@ -122,6 +109,30 @@ fun OnboardingScreen1(onAdminClick: () -> Unit = {}) {
                 color = Color.White,
                 textAlign = TextAlign.Center
             )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 64.dp)
+        ) {
+            Button(
+                onClick = { /*TODO Implement Booking*/ },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFFD700),
+                    contentColor = Color.Black
+                ),
+                shape = RoundedCornerShape(16.dp),
+            ) {
+                Text(
+                    text = "Book a Ride",
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                )
+            }
         }
     }
 }
